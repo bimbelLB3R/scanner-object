0f9119d819a85d1d156a9a64307fd793d42e9775
bimbellb3r <bimbellb3r@gmail.com>
2023-11-20 14:20:50 +0800

    initial commit

---
+++ app/scan/page.js
@@ -0,0 +1,9 @@
+import QRScanner from "@/components/Scan";
+
+export default function Page() {
+  return (
+    <>
+      <QRScanner />
+    </>
+  );
+}
---
+++ components/Scan.js
@@ -0,0 +1,30 @@
+"use client";
+import React, { useRef, useState } from "react";
+import QrScanner from "qr-scanner";
+
+const QRScanner = () => {
+  const videoRef = useRef(null);
+  const [result, setResult] = useState("No result");
+
+  const startScanner = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+      QrScanner.scanImage(videoRef.current)
+        .then((result) => setResult(result))
+        .catch((error) => console.error(error));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <video ref={videoRef} autoPlay playsInline></video>
+      <button onClick={startScanner}>Start Scanner</button>
+      <p>{result}</p>
+    </div>
+  );
+};
+
+export default QRScanner;
