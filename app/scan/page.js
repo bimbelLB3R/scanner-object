061e36632eb31774860a597abaee4c5d247c82ae
bimbellb3r <bimbellb3r@gmail.com>
2023-11-21 19:37:29 +0800

    styling audio

--- app/scan/page.js
+++ app/scan/page.js
@@ -2,8 +2,8 @@ import Scan2 from "@/components/Scan2";
 
 export default function Page() {
   return (
-    <>
+    <div className="bg-[url('/image/bgsatt.jpeg')] bg-cover bg-no-repeat">
       <Scan2 />
-    </>
+    </div>
   );
 }
