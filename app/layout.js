2c4e741735e7087fdd311cf9c438b649b1ff0275
bimbellb3r <bimbellb3r@gmail.com>
2023-11-23 18:09:09 +0800

    metadata icons

--- app/layout.js
+++ app/layout.js
@@ -7,15 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Sekolah Alam SATT",
   description: "Belajar-Berpetualang-Bermakna",
-  openGraph: {
-    images: [
-      {
-        url: "/image/gurusatt.jpeg",
-        width: 500, // Sesuaikan dengan lebar gambar
-        height: 500, // Sesuaikan dengan tinggi gambar
-        alt: "satt", // Gantilah dengan deskripsi yang sesuai
-      },
-    ],
+  icons: {
+    icon: ["/logosatt.png"],
   },
 };
 
