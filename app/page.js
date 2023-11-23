f6a33a7b5ab5c913969b993a8374d9a399433ac7
bimbellb3r <bimbellb3r@gmail.com>
2023-11-23 16:12:20 +0800

    edit bg dan wa

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import TypingEffect from "@/components/TypingText";
 import Formulir from "@/components/Formulir";
+import Wa from "@/components/Wa";
 
 export default function Home() {
   const [isYoutubePopupOpen, setYoutubePopupOpen] = useState(false);
@@ -26,7 +27,7 @@ export default function Home() {
     setFormulirPopupOpen(false);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pl-16 pr-16 pb-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pl-16 pr-16 pb-24 bg-[url('/image/bambu.jpeg')] bg-cover bg-no-repeat">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <code className="font-mono font-bold">
@@ -68,7 +69,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left text-green-800">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left text-green-900">
         <a
           href="/scan"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -80,9 +81,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Eksplorasi Suara Alam.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm `}>Eksplorasi Suara Alam.</p>
         </a>
 
         <a
@@ -97,7 +96,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[30ch] text-sm `}>
             Mengenal lebih dekat Sekolah Alam Tanjug Tabalong
           </p>
         </a>
@@ -113,7 +112,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[30ch] text-sm `}>
             Film-film dokumenter di SATT
           </p>
         </a>
@@ -130,11 +129,12 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[30ch] text-sm `}>
             Formulir pendaftaran siswa baru secara on line
           </p>
         </a>
       </div>
+      <Wa />
       {isYoutubePopupOpen && (
         <div className="absolute top-0 z-20  w-full bg-green-900/50">
           <div className="flex items-center justify-center h-screen m-auto">
